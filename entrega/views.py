b519d94569daa41e00fae05ac0ed1f18555b6477
VictorHuaraz <victorhuarazcornelio@gmail.com>
2023-03-14 21:03:43 -0500

    segunda commit de mi proyecto

--- entrega/views.py
+++ entrega/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from SocialTravel.models import Post
+from SocialTravel.forms import PostForm
 
-# Create your views here.
+def index(request):
+    return render(request, "SocialTravel/index.html")
+
+
+def mostrar_posts(request):
+    context = {
+         "posts": Post.objects.all(),
+         "form": PostForm(),
+         }
+
+    
+    return render(request, "SocialTravel/admin_post.html", context)
+
+
+def agregar_post(request):
+    post_form = PostForm(request.POST)
+    post_form.save()
+    context = {
+         "posts": Post.objects.all(),
+         "form": PostForm(),
+         }
+
+    return render(request, "SocialTravel/admin_post.html", context)
+
+
+def buscar_post(request):
+    criterio = request.GET.get("criterio")
+    context = { "posts": Post.objects.filter(carousel_caption_title__icontains=criterio).all()}
+    return render(request, "SocialTravel/admin_post.html", context)
